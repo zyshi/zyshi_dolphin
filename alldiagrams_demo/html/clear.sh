4b27ccc0ed21ccee0693c98505c49a649b5bc22b
zyshi <zyshi@umich.edu>
2015-02-05 22:07:48 -0500

    bash file for clearing all data

--- alldiagrams_demo/html/clear.sh
+++ alldiagrams_demo/html/clear.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-ls -l
-echo "Removing data files"
-rm *.tsv
-rm barplot_*.html*
-rm scatterplot_*.html*
-rm static/data*.tsv
-ls -l
+echo "Removing auto visual plots htmls"
+rm templates/auto_visual_*.html
+echo "Removing supporting graph js files"
+rm -r static/js/graph_js
+echo "Removing supporting d3 data files"
+rm -r static/data/*
+echo "=== Clearing Finshed ==="
